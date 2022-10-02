a7037f5a8651a3d458c9603515c93dbe88616a8c
Waldy <95896162+TurquoisePenguin7@users.noreply.github.com>
2022-10-02 14:52:10 -0400

    Add files via upload

---
+++ newclean.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import shutil
+
+
+downloads_location = Path.home() / "Downloads"
+file_extensions = ['mp4', 'webm', 'jpeg', 'jpg', 'png', 'pdf', '7z', 'bz2', 'gz', 'zip', 'gif']
+video_location = Path.home() / "Videos"
+image_location = Path.home() / "Pictures"
+pdf_location = Path.home() / "Documents" / 'pdfs'
+zip_location = Path.home() / "Documents" / 'zips'
+
+for extension in file_extensions:
+   for file in downloads_location.glob(f"*.{extension}"):
+      match extension:
+         case 'mp4' | 'webm':
+            shutil.move(file, video_location)
+         case 'png' | 'jpeg' | 'gif' | 'jpg':
+            shutil.move(file, image_location)
+         case 'pdf':
+            shutil.move(file, pdf_location)
+         case '7z' | 'bz2' | 'gz' | 'zip':
+            shutil.move(file, zip_location)
+
